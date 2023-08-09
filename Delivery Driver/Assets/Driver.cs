f537709000a192635862772f8fb7c912572ab08b
yrwshj2s <gauravsharma3941@hotmail.com>
2023-08-09 17:36:57 +0800

    Added script and serialize

---
+++ Delivery Driver/Assets/Driver.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,steerSpeed);
+        transform.Translate(0,moveSpeed,0);
+    }
+}
